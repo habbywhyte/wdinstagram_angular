617f2b799bc1a6a91ece8bfe8be1d13b5d5b6b43
Habby Whyte <bahybaj@hotmail.com>
2016-05-02 22:59:00 -0400

    adds data and controller

--- js/app.js
+++ js/app.js
@@ -1,5 +1,48 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+  .module("wdinstagram", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("PostIndexController", PostIndexControllerFunc)
+  .controller("PostShowController", PostShowControllerFunc)
+  .factory("PostFactory", PostFactoryFunc);
+
+  var posts = [
+    {author: "Tyler"},
+    {author: "Andy"},
+    {author: "Adam"}
+  ]
+
+  function PostIndexControllerFunc() {
+    var indexVm = this;
+    indexVm.posts = posts;
+  }
+
+
+function RouterFuction($stateProvider) {
+  $stateProvider
+    .state("postIndex", {
+      url: "/posts",
+      templateUrl: "js/index.html",
+      controller: "PostIndexController",
+      controllerAs: "indexVm"
+    })
+    .state("PostsShow", {
+        url: "/posts/:id",
+        templateUrl: "js/show.html",
+        controller: "PostShowController",
+        controllerAs: "showVm"
+    });
+  }
+
+
+}
+
+
 })();
